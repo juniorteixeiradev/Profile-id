3e3de523593577a95923061f3a669c2d84dc8f89
Teixeirajrwd <teixeirajrwd@gmail.com>
2023-08-25 18:27:33 -0300

    Foi adicionado uma suavização na troca dos avatares

--- switch.js
+++ switch.js
@@ -4,11 +4,18 @@ const pegarHtml = document.querySelector("body");
 const pegarImg = document.querySelector("#avatar")
     pegarBotao.addEventListener("click", function() {
       pegarHtml.classList.toggle("light");
-      
       if(pegarHtml.classList.contains("light")){
-        pegarImg.setAttribute('src', './assets/avatar-light.png');
+        pegarImg.style.opacity = 0;
+          setTimeout(() => {
+            pegarImg.setAttribute('src', './assets/avatar-light.png');
+            pegarImg.style.opacity = 1;
+        }, 300)
       } else {
-        pegarImg.setAttribute('src', './assets/avatar.png');
+        pegarImg.style.opacity = 0;
+          setTimeout(() => {
+            pegarImg.setAttribute('src', './assets/avatar.png');
+            pegarImg.style.opacity = 1;
+        }, 300)
       }
       
 
